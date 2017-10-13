bdeb54a313102a7c2a7430e3d17cc6d0328cfa35
Flakkles <adrien.jablonski@etu.univ-valenciennes.fr>
2017-10-13 11:17:32 +0200

    Add class Sac

--- src/uvhc/istv/scrabble/Sac.java
+++ src/uvhc/istv/scrabble/Sac.java
@@ -1,61 +1,61 @@
 package uvhc.istv.scrabble;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Sac {
 
 	public ArrayList<Lettre> sac;
 
-	public Sac(ArrayList<Lettre> sac) {
-		this.sac = sac;
-	}
-	
-	
-	public void generer2(Lettre l,int n){
-	for(int i=0;i<n;i++)
-		this.add(l);
-	}
-	
-	public ArrayList<Lettre> generer(){
-		
+	public Sac() {
 		ArrayList<Lettre> sac = new ArrayList<Lettre>();
-		sac.generer2(Lettre.A,9);
-		sac.generer2(B,2);
-		sac.generer2(C,2);
-		sac.generer2(D,3);
-		sac.generer2(E,15);
-		sac.generer2(F,2);
-		sac.generer2(G,2);
-		sac.generer2(H,2);
-		sac.generer2(I,8);
-		sac.generer2(J,1);
-		sac.generer2(K,1);
-		sac.generer2(L,5);
-		sac.generer2(M,3);
-		sac.generer2(N,6);
-		sac.generer2(O,6);
-		sac.generer2(P,2);
-		sac.generer2(Q,1);
-		sac.generer2(R,6);
-		sac.generer2(S,6);
-		sac.generer2(T,6);
-		sac.generer2(U,6);
-		sac.generer2(V,2);
-		sac.generer2(W,1);
-		sac.generer2(X,1);
-		sac.generer2(Y,1);
-		sac.generer2(Z,1);
-		sac.generer2(JOKER,2);
-		return sac;
 	}
-	
-	public ArrayList<Lettre> melanger(ArrayList<Lettre> sac){
-		
-		ArrayList<Lettre> nouvelle = new ArrayList();
-		Collections.shuffle(nouvelle);
-		return nouvelle;
-			
+
+	public void generer2(Lettre l, int n) {
+		for (int i = 0; i < n; i++)
+			this.add(l);
 	}
-	
-	
+
+	public void generer() {
+
+		sac.generer2(Lettre.A, 9);
+		sac.generer2(Lettre.B, 2);
+		sac.generer2(Lettre.C, 2);
+		sac.generer2(Lettre.D, 3);
+		sac.generer2(Lettre.E, 15);
+		sac.generer2(Lettre.F, 2);
+		sac.generer2(Lettre.G, 2);
+		sac.generer2(Lettre.H, 2);
+		sac.generer2(Lettre.I, 8);
+		sac.generer2(Lettre.J, 1);
+		sac.generer2(Lettre.K, 1);
+		sac.generer2(Lettre.L, 5);
+		sac.generer2(Lettre.M, 3);
+		sac.generer2(Lettre.N, 6);
+		sac.generer2(Lettre.O, 6);
+		sac.generer2(Lettre.P, 2);
+		sac.generer2(Lettre.Q, 1);
+		sac.generer2(Lettre.R, 6);
+		sac.generer2(Lettre.S, 6);
+		sac.generer2(Lettre.T, 6);
+		sac.generer2(Lettre.U, 6);
+		sac.generer2(Lettre.V, 2);
+		sac.generer2(Lettre.W, 1);
+		sac.generer2(Lettre.X, 1);
+		sac.generer2(Lettre.Y, 1);
+		sac.generer2(Lettre.Z, 1);
+		sac.generer2(Lettre.JOKER, 2);
+	}
+
+	public void melanger() {
+
+		Collections.shuffle(sac);
+	}
+
+	public Lettre tirer() {
+		Lettre lettre = sac.get(0);
+		sac.remove(0);
+		return lettre;
+	}
+
 }
